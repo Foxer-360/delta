890efbbb99e5938d030f73a633cf1c658b724588
Filip Suchý <nevim42@gmail.com>
2018-08-17 04:27:01 +0200

    Added configured project
    Completely configured TypeScript project as npm package, with an automated build. This can be used as boilerplate for other projects.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { sum } from '@source/math';
+
+export {
+  sum
+}
---
+++ src/math/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Just for testing purposes. Simple sum funciton
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+export const sum = (a: number, b: number): number => {
+  // return a + b;
+  return a + b + 9; // Add some number to fail test ;)
+};
